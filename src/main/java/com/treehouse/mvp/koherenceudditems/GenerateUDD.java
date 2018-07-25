1cd75a3b44923da07956bfcdff00370a6b19932a
hanus <hanus@DESKTOP-MTH3P9U>
2018-07-25 21:48:15 +0200

    Init

--- src/main/java/com/treehouse/mvp/koherenceudditems/GenerateUDD.java
+++ src/main/java/com/treehouse/mvp/koherenceudditems/GenerateUDD.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.treehouse.mvp.koherenceudditems;
 
 import java.util.ArrayList;
@@ -11,7 +6,7 @@ import java.util.Random;
 
 /**
  *
- * @author hanus
+ * @author terner
  */
 public class GenerateUDD {
     private Random rnd;
