a31b447632ef12f70e9c59aacf5c7440f92911b7
terner-84 <37267094+terner-84@users.noreply.github.com>
2018-07-25 21:53:07 +0200

    Update Koherence.java

--- src/main/java/com/treehouse/mvp/koherenceudditems/Koherence.java
+++ src/main/java/com/treehouse/mvp/koherenceudditems/Koherence.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.treehouse.mvp.koherenceudditems;
 
 /**
  *
- * @author hanus
+ * @author terner
  */
 public class Koherence {
     public static void main(String[] args) {
